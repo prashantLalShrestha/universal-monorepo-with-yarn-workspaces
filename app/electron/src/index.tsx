e4c884744b6569d0539a5974937d758faf6c9295
Prashant Shrestha <prashantlurvs@gmail.com>
2022-01-26 14:50:13 +0545

    Created monorepo with React-Native for mobile, web, macos, electron, browser-ext

---
+++ app/electron/src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { App } from '@app/shared';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
